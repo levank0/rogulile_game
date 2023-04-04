b771ac02c446ab6fe3458db39ff39ff66ca036e9
levank0 <levkovalyov2@gmail.com>
2023-04-04 23:36:43 +1000

    добавил наработки мурата

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+
+{
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") > 0){
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
+        } 
+        
+    }
+}
