926a41eb2e00548e3d4b4df7c5dad3c7a6505289
levank0 <levkovalyov2@gmail.com>
2023-04-05 01:24:44 +1000

    add generation map

---
+++ Assets/scripts/Map.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public List<GameObject> roomPrefabs; //список макетов комнат
+    public int numberOfRooms; //количество комнат на карте
+    public float roomWidth = 50; //ширина каждой комнаты
+    public float roomHeight = 50; //высота каждой комнаты
+    public bool showGizmos; //отображение гизмо
+
+
+    private List<GameObject> spawnedRooms = new List<GameObject>(); // список созданных комнат
+    private void Start()
+    {
+        GenerMap();
+    }
+
+    private void GenerMap()
+    {
+        for (int i = 0; i < numberOfRooms; i++)
+        {
+
+
+            int randomI = Random.Range(0, 3);
+            if (randomI == 0)
+            {
+                
+                
+
+                    //выбираем случайный макет комнаты
+                    int randomIndex = Random.Range(0, roomPrefabs.Count);
+                    GameObject newRoom = Instantiate(roomPrefabs[randomIndex]);
+                    float roomPositionX = i * roomWidth;
+                    float roomPositionY = 0f /*i* roomHeight*/;
+                    newRoom.transform.position = new Vector2(roomPositionX, roomPositionY); //размещаем комнаты на карте
+                    spawnedRooms.Add(newRoom); //добавляем созданную комнату в список
+                
+            }
+            else if (randomI == 1)
+            {
+                
+
+                    //выбираем случайный макет комнаты
+                    int randomIndex = Random.Range(0, roomPrefabs.Count);
+                    GameObject newRoom = Instantiate(roomPrefabs[randomIndex]);
+                    float roomPositionX = -i * roomWidth;
+                    float roomPositionY = 0f /*i* roomHeight*/;
+                    newRoom.transform.position = new Vector2(roomPositionX - i* roomWidth, roomPositionY); //размещаем комнаты на карте
+                    spawnedRooms.Add(newRoom); //добавляем созданную комнату в список
+                
+            }
+            else if (randomI == 2)
+            {
+                
+
+                    //выбираем случайный макет комнаты
+                    int randomIndex = Random.Range(0, roomPrefabs.Count);
+                    GameObject newRoom = Instantiate(roomPrefabs[randomIndex]);
+                    float roomPositionX = 0f;
+                    float roomPositionY = i * roomHeight;
+                    newRoom.transform.position = new Vector2(roomPositionX, roomPositionY); //размещаем комнаты на карте
+                    spawnedRooms.Add(newRoom); //добавляем созданную комнату в список
+                
+            }
+            else if (randomI == 3)
+            {
+                
+                
+
+                    //выбираем случайный макет комнаты
+                    int randomIndex = Random.Range(0, roomPrefabs.Count);
+                    GameObject newRoom = Instantiate(roomPrefabs[randomIndex]);
+                    float roomPositionX = 0f;
+                    float roomPositionY = -i * roomHeight;
+                    newRoom.transform.position = new Vector2(roomPositionX, roomPositionY); //размещаем комнаты на карте
+                    spawnedRooms.Add(newRoom); //добавляем созданную комнату в список
+               
+            }
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!showGizmos) return;
+
+        for (int i = 0; i < spawnedRooms.Count - 1; i++)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(spawnedRooms[i].transform.position, spawnedRooms[i + 1].transform.position);
+        }
+    }
+}
+/*for (int i = 1; i < spawnedRooms.Count; i++)
+{
+    //выбираем случайную точку из предыдущей комнаты и текущей
+    Vector2 prevRoomEnd = spawnedRooms[i - 1].GetComponentInChildren<Room>().GetRandomEndPoint();
+    Vector2 currentRoomStart = spawnedRooms[i].GetComponentInChildren<Room>().GetRandomStartPoint();
+
+    //создаем туннель
+    GameObject tunnel = new GameObject("Tunnel");
+    LineRenderer lineRenderer = tunnel.AddComponent<LineRenderer>();
+    lineRenderer.startWidth = 0.1f;
+    lineRenderer.endWidth = 0.1f;
+    lineRenderer.material.color = Color.gray;
+    lineRenderer.SetPositions(new Vector3[] { prevRoomEnd, currentRoomStart });
+}*/
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed; // скорость перемещения квадрата
+    public float xMin ; // минимальное значение координаты X
+    public float xMax ; // максимальное значение координаты X
+    public float yMin ; // минимальное значение координаты Y
+    public float yMax ; // максимальное значение координаты Y
+
+    public GameObject Bullet;
+    public Transform BulletPosition;
+    public float Period = 1;
+    float timerFire;
+
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal"); // получаем значения клавиш влево/вправо
+        float verticalInput = Input.GetAxis("Vertical"); // получаем значения клавиш вверх/вниз
+
+        Vector2 moveDirection = new Vector2(horizontalInput, verticalInput); // создаем вектор движения
+        transform.Translate(moveDirection * speed * Time.deltaTime); // перемещаем объект
+
+        // ограничиваем координаты
+        float xPosition = Mathf.Clamp(transform.position.x, xMin, xMax);
+        float yPosition = Mathf.Clamp(transform.position.y, yMin, yMax);
+        transform.position = new Vector3(xPosition, yPosition, transform.position.z);
+
+        if (Input.GetAxis("Fire1") > 0 && timerFire >= Period)
+        {
+            Instantiate(Bullet, BulletPosition);
+            timerFire = 0;
+        }
+        timerFire = timerFire + Time.deltaTime;timerFire = timerFire + Time.deltaTime;
+    }
+}
